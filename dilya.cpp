1472f165d9e0a5a382d98bc104735b86a05fd1e8
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-10-09 10:52:31 +0500

    Update dilya.cpp

--- dilya.cpp
+++ dilya.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <sstream>
 #include <iomanip>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -22,12 +22,24 @@ struct Friend
 // Ограничения по ширине для столбцов
 const int COLUMN_WIDTH = 15;
 
-// Функция для обрезки строки, если она превышает заданную ширину
-string trimString(const string& str, int width) {
-    if (str.length() <= width) {
-        return str;
-    } else {
-        return str.substr(0, width - 3) + "...";
+// Функция для разбивки строки на подстроки длиной не более COLUMN_WIDTH
+vector<string> splitString(const string& str, int width) {
+    vector<string> lines;
+    int start = 0;
+    while (start < str.length()) {
+        int length = min(width, static_cast<int>(str.length()) - start);
+        lines.push_back(str.substr(start, length));
+        start += length;
+    }
+    return lines;
+}
+
+// Функция для печати строк столбцов в несколько строк, если данные длинные
+void printMultilineColumn(const vector<string>& lines) {
+    for (size_t i = 0; i < lines.size(); ++i) {
+        if (i > 0) cout << "| " << setw(COLUMN_WIDTH) << left << lines[i] << " |";
+        else cout << setw(COLUMN_WIDTH) << left << lines[i] << " |";
+        if (i < lines.size() - 1) cout << "\n";
     }
 }
 
@@ -56,15 +68,34 @@ void inputFriend(Friend &f)
 
 // Функция для печати информации о друге
 void printFriend(const Friend &f) {
-    cout << "| " 
-         << left << setw(COLUMN_WIDTH) << trimString(f.lastName, COLUMN_WIDTH) << " | "
-         << left << setw(COLUMN_WIDTH) << trimString(f.firstName, COLUMN_WIDTH) << " | "
-         << left << setw(COLUMN_WIDTH) << trimString(f.middleName, COLUMN_WIDTH) << " | "
-         << right << setw(2) << setfill('0') << f.day << "."
-         << setw(2) << setfill('0') << f.month << "."
-         << f.year << " | "
-         << left << setw(COLUMN_WIDTH) << trimString(f.address, COLUMN_WIDTH) << " | "
-         << left << setw(COLUMN_WIDTH) << trimString(f.phone, COLUMN_WIDTH) << " |\n";
+    vector<string> lastNameLines = splitString(f.lastName, COLUMN_WIDTH);
+    vector<string> firstNameLines = splitString(f.firstName, COLUMN_WIDTH);
+    vector<string> middleNameLines = splitString(f.middleName, COLUMN_WIDTH);
+    vector<string> addressLines = splitString(f.address, COLUMN_WIDTH);
+    vector<string> phoneLines = splitString(f.phone, COLUMN_WIDTH);
+    
+    // Определяем максимальное количество строк для данного друга
+    size_t maxLines = max({lastNameLines.size(), firstNameLines.size(), middleNameLines.size(), addressLines.size(), phoneLines.size(), 1UL});
+
+    for (size_t i = 0; i < maxLines; ++i) {
+        cout << "| ";
+        printMultilineColumn(i < lastNameLines.size() ? vector<string>{lastNameLines[i]} : vector<string>{""});
+        printMultilineColumn(i < firstNameLines.size() ? vector<string>{firstNameLines[i]} : vector<string>{""});
+        printMultilineColumn(i < middleNameLines.size() ? vector<string>{middleNameLines[i]} : vector<string>{""});
+
+        // Печатаем дату рождения только на первой строке
+        if (i == 0) {
+            cout << right << setw(2) << setfill('0') << f.day << "."
+                 << setw(2) << setfill('0') << f.month << "."
+                 << f.year << " | ";
+        } else {
+            cout << setw(12) << " " << " | "; // Пустое место для выравнивания
+        }
+
+        printMultilineColumn(i < addressLines.size() ? vector<string>{addressLines[i]} : vector<string>{""});
+        printMultilineColumn(i < phoneLines.size() ? vector<string>{phoneLines[i]} : vector<string>{""});
+        cout << "\n";
+    }
 }
 
 // Функция для печати всех друзей
