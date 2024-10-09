017523c482466980fd121a9170216900b6ac6155
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-10-09 11:08:50 +0500

    Update dilya.cpp

--- dilya.cpp
+++ dilya.cpp
@@ -6,8 +6,7 @@
 using namespace std;
 
 // Структура для хранения данных о друге
-struct Friend 
-{
+struct Friend {
     string lastName;     
     string firstName;    
     string middleName;   
@@ -19,8 +18,7 @@ struct Friend
 };
 
 // Функция для ввода данных о друге
-void inputFriend(Friend &f) 
-{
+void inputFriend(Friend &f) {
     cout << "Введите данные о друге:\n";
     cout << "  Фамилия: ";
     getline(cin, f.lastName);
@@ -34,7 +32,7 @@ void inputFriend(Friend &f)
     cin >> f.month;
     cout << "  Год рождения: ";
     cin >> f.year;
-    cin.ignore();  // Очищаем буфер
+    cin.ignore();
     cout << "  Адрес: ";
     getline(cin, f.address);
     cout << "  Телефон: ";
@@ -43,31 +41,30 @@ void inputFriend(Friend &f)
 
 // Функция для печати одного друга
 void printFriend(const Friend &f, int widths[]) {
-    cout << "| " << setw(widths[0]) << f.lastName 
-         << " | " << setw(widths[1]) << f.firstName 
-         << " | " << setw(widths[2]) << f.middleName 
-         << " | " << setw(widths[3]) << f.day << "." << setw(2) << f.month << "." << f.year 
-         << " | " << setw(widths[4]) << f.address 
-         << " | " << setw(widths[5]) << f.phone << " |\n";
+    cout << "| " << left << setw(widths[0]) << f.lastName 
+         << " | " << left << setw(widths[1]) << f.firstName 
+         << " | " << left << setw(widths[2]) << f.middleName 
+         << " | " << right << setw(2) << setfill('0') << f.day << "." 
+         << setw(2) << setfill('0') << f.month << "." << setw(4) << setfill(' ') << f.year
+         << " | " << left << setw(widths[4]) << f.address 
+         << " | " << left << setw(widths[5]) << f.phone << " |\n";
 }
 
 // Функция для нахождения максимальной ширины каждой колонки
 void findMaxWidths(const list<Friend> &friends, int widths[]) {
-    widths[0] = 8; // "Фамилия"
-    widths[1] = 4; // "Имя"
-    widths[2] = 8; // "Отчество"
-    widths[3] = 13; // "Дата рождения"
-    widths[4] = 6; // "Адрес"
-    widths[5] = 7; // "Телефон"
+    widths[0] = max(8, (int)string("Фамилия").length());
+    widths[1] = max(4, (int)string("Имя").length());
+    widths[2] = max(8, (int)string("Отчество").length());
+    widths[3] = string("Дата рождения").length();
+    widths[4] = max(6, (int)string("Адрес").length());
+    widths[5] = max(7, (int)string("Телефон").length());
     
     for (const auto &f : friends) {
-        if (f.lastName.length() > widths[0]) widths[0] = f.lastName.length();
-        if (f.firstName.length() > widths[1]) widths[1] = f.firstName.length();
-        if (f.middleName.length() > widths[2]) widths[2] = f.middleName.length();
-        string dateOfBirth = to_string(f.day) + "." + to_string(f.month) + "." + to_string(f.year);
-        if (dateOfBirth.length() > widths[3]) widths[3] = dateOfBirth.length();
-        if (f.address.length() > widths[4]) widths[4] = f.address.length();
-        if (f.phone.length() > widths[5]) widths[5] = f.phone.length();
+        widths[0] = max(widths[0], (int)f.lastName.length());
+        widths[1] = max(widths[1], (int)f.firstName.length());
+        widths[2] = max(widths[2], (int)f.middleName.length());
+        widths[4] = max(widths[4], (int)f.address.length());
+        widths[5] = max(widths[5], (int)f.phone.length());
     }
 }
 
@@ -82,13 +79,13 @@ void printAllFriends(const list<Friend> &friends) {
     findMaxWidths(friends, widths);
 
     // Печатаем заголовок
-    cout << "| " << setw(widths[0]) << "Фамилия" 
-         << " | " << setw(widths[1]) << "Имя" 
-         << " | " << setw(widths[2]) << "Отчество" 
-         << " | " << setw(widths[3]) << "Дата рождения" 
-         << " | " << setw(widths[4]) << "Адрес" 
-         << " | " << setw(widths[5]) << "Телефон" << " |\n";
-    cout << string(2 + widths[0] + 3 + widths[1] + 3 + widths[2] + 3 + widths[3] + 3 + widths[4] + 3 + widths[5] + 2, '-') << "\n";
+    cout << "| " << left << setw(widths[0]) << "Фамилия" 
+         << " | " << left << setw(widths[1]) << "Имя" 
+         << " | " << left << setw(widths[2]) << "Отчество" 
+         << " | " << left << setw(widths[3]) << "Дата рождения" 
+         << " | " << left << setw(widths[4]) << "Адрес" 
+         << " | " << left << setw(widths[5]) << "Телефон" << " |\n";
+    cout << string(3 + widths[0] + widths[1] + widths[2] + widths[3] + widths[4] + widths[5] + 13, '-') << "\n";
 
     for (const auto &f : friends) {
         printFriend(f, widths);
@@ -96,15 +93,14 @@ void printAllFriends(const list<Friend> &friends) {
 }
 
 // Функция для поиска и вывода друзей, родившихся в заданном месяце
-void findFriendsByMonth(const list<Friend> &friends, int month) 
-{
+void findFriendsByMonth(const list<Friend> &friends, int month) {
     bool found = false;
+    int widths[6];
+    findMaxWidths(friends, widths);
     cout << "Друзья, родившиеся в месяце " << month << ":\n";
     for (const auto &f : friends) {
         if (f.month == month) {
             found = true;
-            int widths[6];
-            findMaxWidths(friends, widths);
             printFriend(f, widths);
         }
     }
