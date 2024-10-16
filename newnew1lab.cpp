985e46f28eaa505d26fba3f13ed6d491223e4bf0
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-10-16 10:11:31 +0500

    Update newnew1lab.cpp

--- newnew1lab.cpp
+++ newnew1lab.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <iomanip> 
 #include <string>
 
 using namespace std;
 
-const int MAX_DISHES = 100; // максимальное количество блюд
+const int MAX_DISHES = 100; // макс количество блюд
 
 struct Dish {
     string name;
@@ -16,7 +17,6 @@ struct Dish {
 Dish dishes[MAX_DISHES];
 int dish_count = 0; // количество блюд в данный момент
 
-//функции для работы с блюдами
 int get_dish_count() {
     return dish_count;
 }
@@ -33,75 +33,62 @@ void add_dish(const Dish& new_dish) {
 void delete_dish(int index) {
     if (index >= 0 && index < get_dish_count()) {
         for (int i = index; i < dish_count - 1; ++i) {
-            dishes[i] = dishes[i + 1]; // сдвигаем элементы
+            dishes[i] = dishes[i + 1]; 
         }
-        dish_count--; // уменьшаем количество блюд
+        dish_count--; 
     } else {
         cout << "Неправильный ввод.\n";
     }
 }
 
-void print_dishes() {
-    for (int i = 0; i < get_dish_count(); ++i) {
-        cout << "Блюдо " << i + 1 << ":\n";
-        cout << "Название: " << dishes[i].name << "\n";
-        cout << "Категория: " << dishes[i].category << "\n";
-        cout << "Калории: " << dishes[i].calories << " ккал\n";
-        cout << "Граммовка: " << dishes[i].weight << (dishes[i].category == "Напитки" ? " мл" : " грамм") << "\n";
-        cout << "Прайс: " << dishes[i].price << " руб. \n\n";
-    }
+void print_table_header() { // заголовок таблицы
+    cout << "| " << left << setw(7) << "№" 
+         << "| " << setw(25) << "Название" 
+         << "| " << setw(20) << "Категория" 
+         << "| " << setw(15) << "Калории" 
+         << "| " << setw(15) << "Вес" 
+         << "| " << setw(20) << "Цена" << " |" << endl;
+    cout << string(90, '-') << endl;
 }
 
-void print_first_courses() {
-    cout << "Основные блюда:\n";
-    for (int i = 0; i < get_dish_count(); ++i) {
-        if (dishes[i].category == "Основные блюда") {
-            cout << "Блюдо " << i + 1 << ":\n";
-            cout << "Название: " << dishes[i].name << "\n";
-            cout << "Калории: " << dishes[i].calories << " ккал\n";
-            cout << "Граммовка: " << dishes[i].weight << " грамм\n";
-            cout << "Прайс: " << dishes[i].price << " руб. \n\n";
-        }
+void print_dishes() { // вывод табл
+    if (get_dish_count() == 0) {
+        cout << "Список блюд пуст.\n";
+        return;
     }
+
+    print_table_header(); 
+    for (int i = 0; i < get_dish_count(); ++i) {
+        cout << "| " << left << setw(5) << i + 1
+             << "| " << setw(20) << dishes[i].name
+             << "| " << setw(20) << dishes[i].category
+             << "| " << setw(10) << dishes[i].calories
+             << "| " << setw(12) << (to_string(dishes[i].weight) + (dishes[i].category == "Напитки" ? " мл" : " г"))
+             << "| " << setw(10) << fixed << setprecision(2) << dishes[i].price << " руб." << " |" << endl;
+    }
+    cout << string(90, '-') << endl; 
+    cout << endl;
 }
 
-void print_second_courses() {
-    cout << "Вторые блюда:\n";
+void print_dishes_by_category(const string& category_name) {
+    bool found = false;
+    print_table_header();
     for (int i = 0; i < get_dish_count(); ++i) {
-        if (dishes[i].category == "Вторые блюда") {
-            cout << "Блюдо " << i + 1 << ":\n";
-            cout << "Нзвание: " << dishes[i].name << "\n";
-            cout << "Калории: " << dishes[i].calories << " ккал\n";
-            cout << "Граммы: " << dishes[i].weight << " грамм\n";
-            cout << "Прайс: " << dishes[i].price << " руб. \n\n";
+        if (dishes[i].category == category_name) {
+            found = true;
+            cout << "| " << left << setw(5) << i + 1
+                 << "| " << setw(20) << dishes[i].name
+                 << "| " << setw(20) << dishes[i].category
+                 << "| " << setw(10) << dishes[i].calories
+                 << "| " << setw(12) << (to_string(dishes[i].weight) + (dishes[i].category == "Напитки" ? " мл" : " г"))
+                 << "| " << setw(10) << fixed << setprecision(2) << dishes[i].price << " руб." << " |" << endl;
         }
     }
-}
-
-void print_desserts() {
-    cout << "Десерты:\n";
-    for (int i = 0; i < get_dish_count(); ++i) {
-        if (dishes[i].category == "Десерты") {
-            cout << "Блюдо " << i + 1 << ":\n";
-            cout << "Название: " << dishes[i].name << "\n";
-            cout << "Калории: " << dishes[i].calories << " ккал\n";
-            cout << "Граммовки: " << dishes[i].weight << " грамм\n";
-            cout << "Прайс: " << dishes[i].price << " руб. \n\n";
-        }
+    cout << string(90, '-') << endl; 
+    if (!found) {
+        cout << "Нет блюд в категории \"" << category_name << "\".\n";
     }
-}
-
-void print_drinks() {
-    cout << "Напитки:\n";
-    for (int i = 0; i < get_dish_count(); ++i) {
-        if (dishes[i].category == "Напитки") {
-            cout << "Блюдо " << i + 1 << ":\n";
-            cout << "Название: " << dishes[i].name << "\n";
-            cout << "Калории: " << dishes[i].calories << " ккал\n";
-            cout << "Объем: " << dishes[i].weight << " мл\n";
-            cout << "Прайс: " << dishes[i].price << " руб. \n\n";
-        }
-    }
+    cout << endl;
 }
 
 int main() {
@@ -113,7 +100,7 @@ int main() {
         cout << "3. Вывод всех блюд\n";
         cout << "4. Вывод основных блюд\n";
         cout << "5. Вывод вторых блюд\n";
-        cout << "6. Вывод дессертов\n";
+        cout << "6. Вывод десертов\n";
         cout << "7. Вывод напитков\n";
         cout << "8. Выход\n";
         cout << "Введите свой выбор: ";
@@ -130,7 +117,7 @@ int main() {
             cout << "Выберите категорию:\n";
             cout << "1 - Основные блюда\n";
             cout << "2 - Вторые блюда\n";
-            cout << "3 - Дессерты\n";
+            cout << "3 - Десерты\n";
             cout << "4 - Напитки\n";
             cout << "Введите номер категории: ";
             cin >> category_choice;
@@ -145,7 +132,7 @@ int main() {
                     new_dish.category = "Вторые блюда";
                     break;
                 case 3:
-                    new_dish.category = "Дессерты";
+                    new_dish.category = "Десерты";
                     break;
                 case 4:
                     new_dish.category = "Напитки";
@@ -179,13 +166,13 @@ int main() {
         } else if (choice == 3) {
             print_dishes();
         } else if (choice == 4) {
-            print_first_courses();
+            print_dishes_by_category("Основные блюда");
         } else if (choice == 5) {
-            print_second_courses();
+            print_dishes_by_category("Вторые блюда");
         } else if (choice == 6) {
-            print_desserts();
+            print_dishes_by_category("Десерты");
         } else if (choice == 7) {
-            print_drinks();
+            print_dishes_by_category("Напитки");
         } else if (choice == 8) {
             break;
         } else {
